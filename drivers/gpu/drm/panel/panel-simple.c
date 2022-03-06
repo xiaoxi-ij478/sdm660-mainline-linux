a6d9205be20924624af3013969061c3e388eb586
iAboothahir <aboothahirpkd@gmail.com>
2022-03-06 15:41:43 +0530

    drm/panel-simple: Add nt36672_tx FHD+ Device - asus zenfone max pro m1 1080p FHD+

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -4610,6 +4610,38 @@ static const struct panel_desc_dsi sony_mermaid_nt36672a_tianma = {
 	.lanes = 4,
 };
 
+static const struct drm_display_mode nt36672_txd_mode = {
+	.clock = (1080 + 122 + 8 + 76) * (2160 + 20 + 4 + 28) * 60 / 1000,
+	.hdisplay = 1080,
+	.hsync_start = 1080 + 122,
+	.hsync_end = 1080 + 122 + 8,
+	.htotal = 1080 + 122 + 8 + 76,
+	.vdisplay = 2160,
+	.vsync_start = 2160 + 20,
+	.vsync_end = 2160 + 20 + 4,
+	.vtotal = 2160 + 20 + 4 + 28,
+	.width_mm = 68,
+	.height_mm = 136,
+};
+
+static const struct panel_desc_dsi nt36672_txd = {
+	.desc = {
+		.modes = &nt36672_txd_mode,
+		.num_modes = 1,
+		.bpc = 8,
+		.size = {
+			.width = 68,
+			.height = 136,
+		},
+		.connector_type = DRM_MODE_CONNECTOR_DSI,
+	},
+	.flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST |
+		 MIPI_DSI_MODE_VIDEO_HSE | MIPI_DSI_MODE_EOT_PACKET |
+		 MIPI_DSI_CLOCK_NON_CONTINUOUS | MIPI_DSI_MODE_LPM,
+	.format = MIPI_DSI_FMT_RGB888,
+	.lanes = 4,
+};
+
 static const struct of_device_id dsi_of_match[] = {
 	{
 		.compatible = "auo,b080uan01",
@@ -4638,6 +4670,9 @@ static const struct of_device_id dsi_of_match[] = {
 	}, {
 		.compatible = "sony,mermaid-nt36672a-tianma",
 		.data = &sony_mermaid_nt36672a_tianma
+	}, {
+		.compatible = "asus,nt36672-txd",
+		.data = &nt36672_txd
 	}, {
 		/* sentinel */
 	}
