faa9928b2e367613f905cf04be640aa92136ffbb
iAboothahir <aboothahirpkd@gmail.com>
2022-03-26 19:50:55 +0530

    drm/panel-simple: Add NT36672_TXD FHD+ IPS LCD Panel
    Signed-off-by: Aboothahir U <aboothahirpkd@gmail.com>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -4508,6 +4508,38 @@ static const struct panel_desc_dsi osd101t2045_53ts = {
 	.lanes = 4,
 };
 
+static const struct drm_display_mode nt36672_txd_mode = {
+	.clock = (1080 + 122 + 8 + 76) * (2160 + 20 + 4 + 28) * 60 / 1000,
+	.hdisplay = 1080,
+	.hsync_start = 1080 + 122,
+	.hsync_end = 1080 + 122 + 8,
+	.htotal = 1080 + 122 + 8 + 76,
+	.vdisplay = 2160,
+	.vsync_start = 2160 + 20,
+	.vsync_end = 2160 + 20 + 4,
+	.vtotal = 2160 + 20 + 4 + 28,
+	.width_mm = 68,
+	.height_mm = 136,
+};
+
+static const struct panel_desc_dsi nt36672_txd = {
+	.desc = {
+		.modes = &nt36672_txd_mode,
+		.num_modes = 1,
+		.bpc = 8,
+		.size = {
+			.width = 68,
+			.height = 136,
+		},
+		.connector_type = DRM_MODE_CONNECTOR_DSI,
+	},
+	.flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST |
+		 MIPI_DSI_MODE_VIDEO_HSE | MIPI_DSI_MODE_NO_EOT_PACKET |
+		 MIPI_DSI_CLOCK_NON_CONTINUOUS | MIPI_DSI_MODE_LPM,
+	.format = MIPI_DSI_FMT_RGB888,
+	.lanes = 4,
+};
+
 static const struct of_device_id dsi_of_match[] = {
 	{
 		.compatible = "auo,b080uan01",
@@ -4530,6 +4562,9 @@ static const struct of_device_id dsi_of_match[] = {
 	}, {
 		.compatible = "osddisplays,osd101t2045-53ts",
 		.data = &osd101t2045_53ts
+    }, {
+		.compatible = "asus,nt36672-txd",
+		.data = &nt36672_txd
 	}, {
 		/* sentinel */
 	}
